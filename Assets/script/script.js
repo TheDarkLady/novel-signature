062fb94d038e1878d6a236621b87d21f8a2880b8
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-08-08 19:43:27 +0530

    galley page done except responsive of tabsection

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -114,8 +114,87 @@ document.addEventListener('DOMContentLoaded', function () {
                 fadeInObserver.observe(element);
             });
 
+            //tabs script 
+            const tabBtn1 = document.getElementById('tab-btn-1');
+            const tabBtn2 = document.getElementById('tab-btn-2');
+            const tabBtn3 = document.getElementById('tab-btn-3')
+            const tabBtn4 = document.getElementById('tab-btn-4')
+            const tabBtn5 = document.getElementById('tab-btn-5')
+            const tab1 = document.getElementById('tab1');
+            const tab2 = document.getElementById('tab2');
+            const tab3 = document.getElementById('tab3');
+            const tab4 = document.getElementById('tab4');
+            const tab5 = document.getElementById('tab5');
 
-        });
+            tabBtn1.classList.add('active');
+            tab1.style.display = 'grid';
+            tab2.style.display = 'none';
+            tab3.style.display = 'none';
+            tab4.style.display = 'none';
+            tab5.style.display = 'none';
+
+
+            tabBtn1.addEventListener('click', () => {
+                tabBtn1.classList.add('active');
+                tabBtn2.classList.remove('active');
+                tabBtn3.classList.remove('active');
+                tabBtn4.classList.remove('active');
+                tabBtn5.classList.remove('active');
+                tab1.style.display = 'grid';
+                tab2.style.display = 'none';
+                tab3.style.display = 'none';
+                tab4.style.display = 'none';
+                tab5.style.display = 'none';
+            });
+            tabBtn2.addEventListener('click', () => {
+                tabBtn1.classList.remove('active');
+                tabBtn2.classList.add('active');
+                tabBtn3.classList.remove('active');
+                tabBtn4.classList.remove('active');
+                tabBtn5.classList.remove('active');
+                tab1.style.display = 'none';
+                tab2.style.display = 'grid';
+                tab3.style.display = 'none';
+                tab4.style.display = 'none';
+                tab5.style.display = 'none';
+            });
+            tabBtn3.addEventListener('click', () => {
+                tabBtn1.classList.remove('active');
+                tabBtn2.classList.remove('active');
+                tabBtn3.classList.add('active');
+                tabBtn4.classList.remove('active');
+                tabBtn5.classList.remove('active');
+                tab1.style.display = 'none';
+                tab2.style.display = 'none';
+                tab3.style.display = 'grid';
+                tab4.style.display = 'none';
+                tab5.style.display = 'none';
+            });
+            tabBtn4.addEventListener('click', () => {
+                tabBtn1.classList.remove('active');
+                tabBtn2.classList.remove('active');
+                tabBtn3.classList.remove('active');
+                tabBtn4.classList.add('active');
+                tabBtn5.classList.remove('active');
+                tab1.style.display = 'none';
+                tab2.style.display = 'none';
+                tab3.style.display = 'none';
+                tab4.style.display = 'grid';
+                tab5.style.display = 'none';
+            });
+            tabBtn5.addEventListener('click', () => {
+                tabBtn1.classList.remove('active');
+                tabBtn2.classList.remove('active');
+                tabBtn3.classList.remove('active');
+                tabBtn4.classList.remove('active');
+                tabBtn5.classList.add('active');
+                tab1.style.display = 'none';
+                tab2.style.display = 'none';
+                tab3.style.display = 'none';
+                tab4.style.display = 'none';
+                tab5.style.display = 'grid';
+            });
+    });
 
 
 
