f172a75312c414d17a688a8af5135f1d0baf013e
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-08-13 13:16:17 +0530

    property page is done

--- Assets/script/propertypage.js
+++ Assets/script/propertypage.js
@@ -3,4 +3,16 @@ document.querySelectorAll('details summary').forEach((summary) => {
         const svg = this.querySelector('svg');
         svg.classList.toggle('rotate-180');
     });
+
+    const img = document.querySelector('.floor-plan-img');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                img.style.transform = 'rotate(0deg)';
+            }
+        });
+    });
+
+    observer.observe(img);
 });
